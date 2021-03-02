5316c31d35a7656e89b31e0dfa13485bedf8a79e
Explore21 <76804530+Explore21@users.noreply.github.com>
2021-03-02 15:08:54 +0900

    Add files via upload

---
+++ vgg_wbn.py
@@ -0,0 +1,86 @@
+import os
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, Input, Conv2D, MaxPooling2D, GlobalAveragePooling2D,BatchNormalization
+from tensorflow.keras.layers import  GlobalMaxPooling2D, Activation, Dropout, Flatten, Dense
+from tensorflow.keras import backend as K
+
+def conv_block(units, dropout=0.2, activation='relu', block=1, layer=1):
+
+    def layer_wrapper(inp):
+        x = Conv2D(units, (3, 3), padding='same', name='block{}_conv{}'.format(block, layer))(inp)
+        #x = BatchNormalization(name='block{}_bn{}'.format(block, layer))(x)
+        x = Activation(activation, name='block{}_act{}'.format(block, layer))(x)
+        x = Dropout(dropout, name='block{}_dropout{}'.format(block, layer))(x)
+        return x
+
+    return layer_wrapper
+
+def dense_block(units, dropout=0.2, activation='relu', name='fc1'):
+
+    def layer_wrapper(inp):
+        x = Dense(units, name=name)(inp)
+        x = BatchNormalization(name='{}_bn'.format(name))(x)
+        x = Activation(activation, name='{}_act'.format(name))(x)
+        x = Dropout(dropout, name='{}_dropout'.format(name))(x)
+        return x
+
+    return layer_wrapper
+        
+
+def VGG16_wBN(input_tensor=None, input_shape=None, classes=1000, conv_dropout=0.1, dropout=0.3, activation='relu'):
+    """Instantiates the VGG16 architecture with Batch Normalization
+    # Arguments
+        input_tensor: Keras tensor (i.e. output of `layers.Input()`) to use as image input for the model.
+        input_shape: shape tuple
+        classes: optional number of classes to classify images
+    # Returns
+        A Keras model instance.
+    """
+    img_input = Input(shape=input_shape) if input_tensor is None else (
+        Input(tensor=input_tensor, shape=input_shape) if not K.is_keras_tensor(input_tensor) else input_tensor
+    )
+
+    # Block 1
+    x = conv_block(32, dropout=conv_dropout, activation=activation, block=1, layer=1)(img_input)
+    x = conv_block(32, dropout=conv_dropout, activation=activation, block=1, layer=2)(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+
+    # Block 2
+    x = conv_block(64, dropout=conv_dropout, activation=activation, block=2, layer=1)(x)
+    x = conv_block(64, dropout=conv_dropout, activation=activation, block=2, layer=2)(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+
+    # Block 3
+    x = conv_block(128, dropout=conv_dropout, activation=activation, block=3, layer=1)(x)
+    x = conv_block(128, dropout=conv_dropout, activation=activation, block=3, layer=2)(x)
+    x = conv_block(128, dropout=conv_dropout, activation=activation, block=3, layer=3)(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+
+    # Block 4
+    x = conv_block(256, dropout=conv_dropout, activation=activation, block=4, layer=1)(x)
+    x = conv_block(256, dropout=conv_dropout, activation=activation, block=4, layer=2)(x)
+    x = conv_block(256, dropout=conv_dropout, activation=activation, block=4, layer=3)(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+
+    # Block 5
+    x = conv_block(256, dropout=conv_dropout, activation=activation, block=5, layer=1)(x)
+    x = conv_block(256, dropout=conv_dropout, activation=activation, block=5, layer=2)(x)
+    x = conv_block(256, dropout=conv_dropout, activation=activation, block=5, layer=3)(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
+
+    # Flatten
+    x = GlobalAveragePooling2D()(x)
+
+    # FC Layers
+    x = dense_block(512, dropout=dropout, activation=activation, name='fc1')(x)
+    x = dense_block(512, dropout=dropout, activation=activation, name='fc2')(x)
+    
+    # Classification block    
+    x = Dense(classes, activation='softmax', name='predictions')(x)
+
+    # Ensure that the model takes into account any potential predecessors of `input_tensor`.
+    inputs = get_source_inputs(input_tensor) if input_tensor is not None else img_input
+
+    # Create model.
+    return Model(inputs, x, name='vgg16_bn')
